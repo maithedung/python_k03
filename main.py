e7f934b029bb2591cc86398f4a9d6a69035cac3a
maithedung <maithedung.js@gmail.com>
2021-03-28 21:44:04 +0700

    oki

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-from fastapi import FastAPI, status
-from pydantic import BaseModel
-from typing import Optional, List
+from fastapi import FastAPI, status, Depends
+from schemals import Blog
+from database import SessionLocal, engine, get_db
+import models
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-    tags: List[str] = []
-
-
-class User(BaseModel):
-    username: str
-    full_name: Optional[str] = None
+models.Base.metadata.create_all(bind=engine)
 
 
 @app.get("/", status_code=status.HTTP_200_OK)
@@ -23,19 +14,27 @@ def root():
     return {"message": "Hello World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get('/blogs')
+def get_all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
 
 
-@app.post("/items/")
-def create_item(item: Item):
-    print(item)
-    return item
+@app.get("/blogs/{id}")
+def read_blog(id: int):
+    return {"blog_id": id}
 
 
+@app.post("/blogs/")
+def create_blog(request: Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    results = {"item_id": item_id, "item": item}
+
+@app.put("/blogs/{id}")
+async def update_blog(id: int, request: Blog):
+    results = {"title": request.title, "content": request.body}
     return results
---
+++ models.py
@@ -0,0 +1,11 @@
+from database import Base
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+
+class Blog(Base):
+    __tablename__ = 'blogs'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    body = Column(String)
+
